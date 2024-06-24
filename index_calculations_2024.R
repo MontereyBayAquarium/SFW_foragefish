de4d0b5c34edb7143e4ba3ae77b29dd51db881f5
rachelzuercher <rachel.zuercher@gmail.com>
2024-06-24 16:16:46 -0400

    add output files

--- index_calculations_2024.R
+++ index_calculations_2024.R
@@ -1,18 +1,4 @@
 
-# Authors: Marjoleine M.H. Roos (marjoleineroos@gmail.com) & Tim E. Essington
-
-# connectance index calculations are based on Smith, A. D., C. J. Brown, 
-#      C. M. Bulman, E. A. Fulton, P. Johnson, I. C. Kaplan, H. Lozano-Montes, 
-#      S. Mackinson, M. Marzloff, and L. J. Shannon. 2011. Impacts of fishing 
-#      low–trophic level species on marine ecosystems. Science 333(6046):1147-1150 
-# mass SURF index calculations are based on Plagányi, É. E., and T. E. Essington. 
-#      2014. When the SURFs up, forage fish are key. Fisheries Research 159:68-74.
-#      doi: 10.1016/j.fishres.2014.05.011
-# energy SURF index calculations are based on Surma, S., E. A. Pakhomov, and T. 
-#      J. Pitcher. 2022. Pacific herring (Clupea pallasii) as a key forage fish 
-#      in the southeastern Gulf of Alaska. Deep Sea Research Part II: Topical 
-#      Studies in Oceanography 196:105001. doi: 10.1016/j.dsr2.2021.105001
-
 #==============================================================================
 #         CALCULATING THE CONNECTANCE, MASS SURF, AND ENERGY SURF INDEX
 #==============================================================================
@@ -192,11 +178,83 @@ write.xlsx(list_of_datasets, file.path(output, paste0(geography, ".xlsx")))    #
 
 #======= STEP 4.2.1. DEFINE QBE ===============================================
 
-QBE[is.na(QBE)] <- 0 
 group.data$QBE<-(t(ECTOT[1,]/group.data$B))                                    # ADD A VECTOR TO THE GROUP DATA FOR THE ENERGY CONSUMPTION/BIOMASS (QBE) CALCULATED BY DIVIDING THE TOTAL ENERGY CONSUMPTION PER GROUP (CALCULATE AT LINES 40-45) BY ITS BIOMASS  
 QBE<-group.data$QBE                                                            # EXTRACT THESE DATA AS SEPERATE VECTOR
 QBE[is.na(QBE)] <- 0                                                           # CONVERT NA VALUES TO ZEROS
 
+
+#======= STEP 4.2.2. CREATE FUNCTION FOR AGGREGATING GROUPS IN DIET MATRICES ================================
+
+
+aggregate.groups<-function(combine.groups,new.groupname,C,EC,B,QB,QBE,p,pE,group.names) {                     # CREATE FUNTION THAT USES THE ASSIGNED PARAMETERS, SOME ARE BEING GENERATED IN LINES BELOW: COMBINE.GROUPS -> 264 AND NEW.GROUPNAME -> 270
+  
+  ngroups=length(B)                                                                                           # DETERMINE NUMBER OF GROUPS FROM NUMBER OF BIOMASSES
+  
+  other.groups=c()                                                                                            # CREATE VECTOR OF GROUPS THAT ARE NOT AGGREGATED
+  for (i in 1:ngroups){                                                                                       # RUN A LOOP WITH NUMBER OF RUNS (i) EQUAL TO # ORIGINAL GROUPS
+    group.index=which(combine.groups==i)                                                                      # DETERMINE WHETHER GROUP i IS GETTING COMBINED (TRUE) OR NOT (FALSE)
+    if (length(group.index)==0){                                                                              # IF GROUP i IS NOT COMBINED (AND THUS INDEX IS FALSE AND LENGTH INDEX =0), THEN
+      other.groups=c(other.groups,i)                                                                          # GROUPS NAME REMAINS UNCHANGED
+    }
+  }
+  
+  
+  #======= CREATE TEMP VECTORS FOR BASIC MODEL PARAMETERS ADDING NEW GROUP =====
+  
+  
+  B.tmp=as.matrix(c(B,sum(B[combine.groups])),nrow=ngroups+1)                                                 # ADD SUMMED BIOMASS OF AGGREGATE GROUP TO THE BOTTOM OF EXISTING LIST OF BIOMASS (B) VALUES 
+  QB.newgroup=sum(QB[combine.groups]*B[combine.groups]/sum(B[combine.groups]))                                # CALCULATE CONSUMPTION/BIOMASS (QB) BY CALCULATING ACTUAL CONSUMPTION PER SEPERATE GROUPS DIVIDED BY BIOMASS OF AGGREGATED GROUP AND SUM THESE
+  QB.tmp=as.matrix(c(QB,QB.newgroup),nrow=ngroups+1)                                                          # ADD CONSUMPTION/BIOMASS (QB) OF AGGREGATE GROUP TO THE BOTTOM OF EXISTING LIST OF QB VALUES
+  QBE.newgroup=sum(QBE[combine.groups]*B[combine.groups]/sum(B[combine.groups]))                              # CALCULATE ENERGY CONSUMPTION/BIOMASS (QBE) BY CALCULATING ACTUAL ENERGY CONSUMPTION PER SEPERATE GROUPS DIVIDED BY BIOMASS OF AGGREGATED GROUP AND SUM THESE
+  QBE.tmp=as.matrix(c(QBE,QBE.newgroup),nrow=ngroups+1)                                                       # ADD ENERGY CONSUMPTION/BIOMASS (QBE) OF AGGREGATE GROUP TO THE BOTTOM OF EXISTING LIST OF QBE VALUES
+  group.names.tmp=as.matrix(c(group.names,new.groupname),nrow=ngroups+1)                                      # ADD GROUP NAME OF AGGREGATE GROUP TO THE BOTTOM OF EXISTING LIST OF GROUP NAMES
+  
+  
+  #======= MASS DIET MATRIX =============================
+  
+  
+  C.tmp.pred=rowSums(C[,combine.groups])                                                                      # CALCULATE ACTUAL CONSUMPTION IN MASS OF EACH PREY BY AGGREGATED GROUP AS PREDATOR BY SUMMING ACTUAL CONSUMPTION OF PREY IN MASS BY SEPERATE GROUPS 
+  p.tmp.pred=C.tmp.pred/sum(C.tmp.pred)                                                                       # CALCULATE DIET PROPORTION (p) OF EACH PREY IN DIET AGGREGATED GROUP BY DIVIDING ACTUAL CONSUMPTION OF THAT PREY IN MASS BY TOTAL ACTUAL MASS CONSUMPTION OF AGGREGATE GROUP
+  p.tmp.new=cbind(p,p.tmp.pred)                                                                               # ADD DIET COMPOSITION (p) OF AGGREGATE GROUP AS COLUMN TO EXISTING DIET MATRIX
+  
+  p.tmp.prey=colSums(p.tmp.new[combine.groups,])                                                              # CALCULATE DIET PROPORTION (p) THAT AGGREGATED GROUP REPRESENT AS PREY FOR EACH PREDATOR BY SUMMING THEIR PROPORTION (p) AS SEPERATE GROUPS
+  p.tmp.new=rbind(p.tmp.new,p.tmp.prey)                                                                       # ADD ROW OF p-VALUES OF AGGREGATED GROUP AS PREY TO BOTTOM OF DIET MATRIX
+  
+  
+  #======= ENERGY DIET MATRIX =============================
+  
+  
+  EC.tmp.pred=rowSums(EC[,combine.groups])                                                                    # CALCULATE ACTUAL CONSUMPTION IN ENERGY OF EACH PREY BY AGGREGATED GROUP AS PREDATOR BY SUMMING ACTUAL CONSUMPTION OF PREY IN ENERGY BY SEPERATE GROUPS 
+  pE.tmp.pred=EC.tmp.pred/sum(EC.tmp.pred)                                                                    # CALCULATE DIET PROPORTION (p) OF EACH PREY IN DIET AGGREGATED GROUP BY DIVIDING ACTUAL CONSUMPTION OF THAT PREY IN ENERGY BY TOTAL ACTUAL ENERGY CONSUMPTION OF AGGREGATE GROUP
+  pE.tmp.new=cbind(pE,pE.tmp.pred)                                                                            # ADD DIET COMPOSITION (pE) OF AGGREGATE GROUP AS COLUMN TO EXISTING DIET MATRIX
+  
+  pE.tmp.prey=colSums(pE.tmp.new[combine.groups,])                                                            # CALCULATE ENERGY DIET PROPORTION (pE) THAT AGGREGATED GROUP REPRESENT AS PREY FOR EACH PREDATOR BY SUMMING THEIR PROPORTION (pE) AS SEPERATE GROUPS
+  pE.tmp.new=rbind(pE.tmp.new,pE.tmp.prey)                                                                    # ADD ROW OF pE-VALUES OF AGGREGATED GROUP AS PREY TO BOTTOM OF DIET MATRIX
+  
+  
+  #======= UPDATE NEW DIET MATRICES WITH NEW GROUP NAMES ==================================   
+  
+  
+  other.groups=c(other.groups,n.groups+1)                                                                     # CREATE VECTOR WITH EXISTING GROUP NAMES PLUS ONE ROW
+  p.new=p.tmp.new[other.groups,other.groups]                                                                  # CREATE NEW DIET MATRIX FOR SURF MASS-BALANCED INCLUDING AGGREGATED GROUP
+  p.new[is.na(p.new)] <- 0                                                                                    # REPLACE NA'S WITH 0'S
+  pE.new=pE.tmp.new[other.groups,other.groups]                                                                # CREATE NEW DIET MATRIX FOR SURF ENERGY-BALANCED INCLUDING AGGREGATED GROUP
+  pE.new[is.na(pE.new)] <- 0                                                                                  # REPLACE NA'S WITH 0'S
+  new.colnames=colnames(p.new)                                                                                # ASSIGN NEW COLUMN NAMES
+  nr=length(new.colnames)                                                                                     # DETERMINE NUMBER OF GROUPS
+  new.colnames[nr]=new.groupname                                                                              # DETERMINE NEW GROUP NAME AS THE LAST ONE
+  colnames(p.new)=new.colnames                                                                                # GIVE NEW DIET MATRIX FOR SURF MASS-BALANCED NEW COLUMN NAMES
+  rownames(p.new)=c()                                                                                         # REMOVE ROW NAMES
+  colnames(pE.new)=new.colnames                                                                               # GIVE NEW DIET MATRIX FOR SURF ENERGY-BALANCED NEW COLUMN NAMES
+  rownames(pE.new)=c()                                                                                        # REMOVE ROW NAMES
+  
+  B.new=B.tmp[other.groups]                                                                                   # ADJUST BIOMASS (B) AS BASIC PARAMETER
+  QB.new=QB.tmp[other.groups]                                                                                 # ADJUST CONSUMPTION/BIOMASS (QB) AS BASIC PARAMETER
+  QBE.new=QBE.tmp[other.groups]                                                                               # ADJUST ENERGY CONSUMPTION/BIOMASS (QBE) AS BASIC PARAMETER
+  group.names.new=group.names.tmp[other.groups]                                                               # ADJUST GROUP NAMES AS BASIC VECTOR
+  
+  Agg.model=list(B=B.new,QB=QB.new,QBE=QBE.new,group.names=group.names.new,p=p.new,pE=pE.new)                 # CREATE A LIST OF THE FUNCTION'S OUTCOMES WITH THE PARAMETERS YOU WANT TO BE PRODUCED 
+}
 
 #======= STEP 4.2.2. DEFINE GROUPS TO AGGREGATE ===============================
 
